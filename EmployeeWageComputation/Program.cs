17f82a188a48a93ec6ed5965a2d37e67705890d0
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-20 21:47:50 +0530

    [RK Nanda] Adding Menu options

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,34 +5,69 @@ class Program
     {
         //START
         Console.WriteLine("Welcome to Employee Wage Computation!");
-
-        //UC1
         Employee emp = new Employee();
-        emp.IsPresent();
-        Console.ReadLine();
+        bool flag = true;
+        int option;
+        while (flag)
+        {
+            Console.WriteLine("Menu->");
+            Console.WriteLine("1.Check if Employee Present or Absent");
+            Console.WriteLine("2.Calculate Daily Wage");
+            Console.WriteLine("3.Check if Employee is PartTime or Full Time");
+            Console.WriteLine("4.Check if Employee is PartTime or Full Time using Switch Case");
+            Console.WriteLine("5.Calculate Monthly Wage of Employee");
+            Console.WriteLine("0.Exit");
+            option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 0:
+                    {
+                        flag = false;
+                        break;
+                    }
+
+                case 1:
+                    {
+                        emp.IsPresent();
+                        break;
+                    }
+
+                case 2:
+                    {
+                        emp.DailyWage();
+                        break;
+                    }
+                case 3:
+                    {
+                        emp.PartTime();
+                        break;
+
+                    }
+                case 4:
+                    {
+                        emp.PartTimeSwtich();
+                        break;
+
+                    }
+                case 5:
+                    {
+                        emp.MonthlyWage();
+                        break;
+
+                    }
+                case 6:
+                    {
+                        emp.TotalWage();
+                        break;
+
+                    }
 
 
-        //UC2
-        emp.DailyWage();
-        Console.ReadLine();
 
-        //UC3
-        Console.WriteLine("UC3:-");
-        emp.PartTime();
-        Console.ReadLine();
+            }
 
-        //UC4
-        emp.PartTimeSwtich();
-        Console.ReadLine();
 
-        //UC5
-        emp.MonthlyWage();
-        Console.ReadLine();
-
-        //UC6
-        emp.TotalWage();
-        Console.ReadLine();
-
+        }
     }
 
 }
